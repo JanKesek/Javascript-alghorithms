60eb97c9c52541292c74bcd6a1de0ef95cb444a8
JanKesek <4chewbaca@gmail.com>
2017-11-23 19:03:34 +0100

    Check if string b is also ending of string a

---
+++ sprawdz czy string b konczy string a.js
@@ -0,0 +1,10 @@
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  var n = str.endsWith(target); 
+  return n;
+}
+
+
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification");
